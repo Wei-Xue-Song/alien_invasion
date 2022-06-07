d34f3a0f2099648ee56e1a7e0c0f228675d87d63
wxs2011133069 <2856141212@qq.com>
2022-06-08 07:11:58 +0800

    创建外星人群

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,9 @@
 import sys
 
 import pygame
-from alien import Alien
+from pygame.event import Event
 
+from alien import Alien
 from bullet import Bullet
 from settings import Settings
 from ship import Ship
@@ -26,33 +27,49 @@ class AlienInvasion:
 
         self._create_fleet()
 
-    def _create_alien(self, alien_number):
+    def get_number_aliens_x(self, alien: Alien):
+        """计算每行可容纳多少个外星人"""
+        alien_width = alien.rect.width
+        screen_width = self.settings.screen_width
+        available_space_x = screen_width - alien_width
+        # 外星人的间距为外星人宽度
+        return available_space_x // (2 * alien_width)
+
+    def get_number_rows(self, alien: Alien):
+        """计算屏幕可容纳多少行外星人"""
+        alien_height = alien.rect.height
+        ship_height = self.ship.rect.height
+        screen_height = self.settings.screen_height
+        available_space_y = screen_height - ship_height - 3 * alien_height
+        return available_space_y // (2 * alien_height)
+
+    def _create_alien(self, row_number: int, alien_number: int):
         """创建一个外星人并将其放置在当前行"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
+        alien.y = alien_height + 2 * alien_height * row_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.y
         self.aliens.add(alien)
 
     def _create_fleet(self):
         """创建外星人群"""
         # 创建一个外星人并计算一行可容纳多少个外星人
-        # 外星人的间距为外星人宽度
         alien = Alien(self)
-        alien_width = alien.rect.width
-        available_space_x = self.settings.screen_width - 2 * alien_width
-        number_aliens_x = available_space_x // (2 * alien_width)
+        number_rows = self.get_number_rows(alien)
+        number_aliens_x = self.get_number_aliens_x(alien)
 
-        # 创建第一行外星人
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(row_number, alien_number)
 
     def _fire_bullet(self):
         """创建一颗子弹并将其加入编组bullets中"""
         if len(self.bullets) < self.settings.bullet_allowed:
             self.bullets.add(Bullet(self))
 
-    def _check_keyup_event(self, event):
+    def _check_keyup_event(self, event: Event):
         if event.key == pygame.K_UP:
             self.ship.moving_up = False
         elif event.key == pygame.K_DOWN:
@@ -62,7 +79,7 @@ class AlienInvasion:
         elif event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
 
-    def _check_keydown_event(self, event):
+    def _check_keydown_event(self, event: Event):
         if event.key == pygame.K_q:
             pygame.quit()
             sys.exit()
